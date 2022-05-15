2e9272f8108ffd207f36ce157d780efdb4967936
IgorGultyaev <igor.gultyaev@mail.ru>
2022-05-15 15:16:21 +0300

    finally

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,99 +1,43 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.imageio.plugins.jpeg.JPEGImageWriter;
+import com.sun.imageio.plugins.jpeg.JPEGImageWriterSpi;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriter;
+import javax.imageio.stream.FileImageOutputStream;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
-import com.sun.imageio.plugins.jpeg.*;
-import javax.imageio.stream.*;
-import java.io.*;
 
 
 public class Main {
 
-    public static final String root = ".\\";
-
-    public static String downloadImageByURL(NASAResponse nasaResponse) throws IOException {
-
-        String fileName = root + nasaResponse.getHdurl()
-                .toString().split("/")[nasaResponse.getHdurl().toString().split("/").length-1];
-
-        BufferedImage img = ImageIO.read(nasaResponse.getHdurl().toURL());
-
-        //            BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-//            for (int i = 0; i < height; i++)
-//                for (int j = 0; j < width; j++)
-//                    bi.setRGB(j, i, pixels[i*width +j]);
-//            return bi;
-//        }
-
-
-
-        // Запись изображения в jpeg-формате
-
-            ImageWriter writer = new JPEGImageWriter(new JPEGImageWriterSpi());
-//            saveToImageFile(writer, fileName);
-
-//        private void saveToImageFile(ImageWriter iw, String fileName) throws IOException {
-
-        writer.setOutput(new FileImageOutputStream(new File(fileName)));
-        writer.write(img);
-        ((FileImageOutputStream) writer.getOutput()).close();
-
-
-
-//        // Формирование BufferedImage из массива pixels
-//        private BufferedImage copyToBufferedImage()  {
-//            BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-//            for (int i = 0; i < height; i++)
-//                for (int j = 0; j < width; j++)
-//                    bi.setRGB(j, i, pixels[i*width +j]);
-//            return bi;
-//        }
-
-
-
-
-//        try (BufferedInputStream in =new BufferedInputStream(nasaResponse.getHdurl().toURL().openStream())){
-//            FileOutputStream fos = new FileOutputStream(fileName){
-//
-//
-//
-//            }
-//        }catch (){
-//
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-
-//        try (BufferedInputStream in = new BufferedInputStream(nasaResponse.getHdurl().toURL().openStream());
-//             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
-//            byte dataBuffer[] = new byte[4092];
-//            int bytesRead;
-//            while ((bytesRead = in.read(dataBuffer, 0, 4092)) != -1) {
-//                fileOutputStream.write(dataBuffer, 0, bytesRead);
-//            }
-//            fileOutputStream.flush();
-//        } catch (IOException e) {
-//            // handle exception
-//        }
-
-        return null;
-    }
-
+    public static final String root = ".\\image\\";
     public static final String keyAPI = "mbyJaX1HmPe6XMTdqhaxkhly8LmDrPhdw1ogbLU3";
     public static ObjectMapper mapper = new ObjectMapper();
 
-    public static void main(String[] args) throws IOException {
+    public static String downloadImageByURL(NASAResponse nasaResponse) throws IOException {
 
+        if (nasaResponse.getMedia_type().equals("image")) {
+            String fileName = root + nasaResponse.getHdurl()
+                    .toString().split("/")[nasaResponse.getHdurl().toString().split("/").length - 1];
+            BufferedImage img = ImageIO.read(nasaResponse.getHdurl().toURL());
+            ImageWriter writer = new JPEGImageWriter(new JPEGImageWriterSpi());
+            writer.setOutput(new FileImageOutputStream(new File(fileName)));
+            writer.write(img);
+            ((FileImageOutputStream) writer.getOutput()).close();
+            return "картинка успешно сохранена в" + fileName;
+        }
+        return "файл не является картинкой";
+    }
+
+    public static void main(String[] args) throws IOException {
 
         CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(RequestConfig.custom()
@@ -114,9 +58,8 @@ public class Main {
         System.out.println(nasaResponse.getHdurl());
         System.out.println(nasaResponse.getUrl());
         request.setURI(nasaResponse.getHdurl());
-        response = httpClient.execute(request);
-        downloadImageByURL(nasaResponse);
 
+        System.out.println(downloadImageByURL(nasaResponse));
     }
 
 
